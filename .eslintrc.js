f8566681d84882ba3d3834785d184d044b46a769
dm4t2 <dm4t2@outlook.com>
2022-04-22 00:19:15 +0200

    chore: use eslint-plugin-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,11 @@ module.exports = {
     node: true
   },
   plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-tsdoc'],
-  ignorePatterns: ['dist/**', 'coverage/**'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  ignorePatterns: ['dist/**', 'coverage/**', 'temp/**'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2020
   },
-  rules: { '@typescript-eslint/ban-ts-ignore': 'off' },
   overrides: [
     {
       files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
